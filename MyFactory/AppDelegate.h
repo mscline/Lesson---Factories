3791dc0043c76b0be12e128dd374f63d33283778
mscline <mychiloscline@gmail.com>
2015-01-09 06:49:23 -0800

    Initial Commit

---
+++ MyFactory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFactory
+//
+//  Created by xcode on 1/9/15.
+//  Copyright (c) 2015 MSCline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
